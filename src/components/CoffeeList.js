a7bb51b27ca192dd80f415201b7796b37b156c57
onurkaymak <onurkaymak@outlook.com>
2023-11-17 10:03:19 -0800

    Create CoffeeList component.

---
+++ src/components/CoffeeList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CoffeeList = (props) => {
+
+
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
+
+CoffeeList.propTypes = {
+
+};
+
+export default CoffeeList;
