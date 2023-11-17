2d450eae0e4133681735adffa0d2f0309d7c2f08
onurkaymak <onurkaymak@outlook.com>
2023-11-17 09:20:34 -0800

    Create ReusableForm for Create and Update functionality.

---
+++ src/utils/ReusableForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ReusableForm = (props) => {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
