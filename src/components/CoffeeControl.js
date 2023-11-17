ea256fcabb032139631559d30673ecb4a690bce4
onurkaymak <onurkaymak@outlook.com>
2023-11-17 15:17:36 -0800

    Create handleEditCoffee method, refactor conditional rendering for the new state.

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NewCoffeeForm from './NewCoffeeForm';
 import CoffeeList from './CoffeeList';
 import CoffeeDetail from './CoffeeDetail';
+import EditCoffeeForm from './EditCoffeeForm';
 
 
 class CoffeeControl extends React.Component {
@@ -11,7 +12,8 @@ class CoffeeControl extends React.Component {
     this.state = {
       mainCoffeeList: [],
       isAddFormVisible: false,
-      selectedCoffee: null
+      selectedCoffee: null,
+      editing: false
     };
   }
 
@@ -42,7 +44,7 @@ class CoffeeControl extends React.Component {
     this.setState({ selectedCoffee }); // ES6 way to set object key-value.
   };
 
-  handleCoffeeSale = (id) => {
+  handleCoffeeSale = (id,) => {
     // When we use the method shown in Help Queue project, selected item changes its position in the list after edit its state, I think that's bad UI experience. 
     //I implemented a way to keep its position at the list after any changes in the state.
 
@@ -62,16 +64,35 @@ class CoffeeControl extends React.Component {
     this.setState({
       mainCoffeeList: newMainCoffeeList // 7 - Update the state without break any state rules.
     })
-
   }
+
+  handleEditClick = () => {
+    this.setState({ editing: true });
+  }
+
+  handleEditCoffee = (coffeeToEdit) => {
+    let selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === coffeeToEdit.id)[0]; // Find the selected coffee to edit.
+    const selectedCoffeeIndex = this.state.mainCoffeeList.indexOf(selectedCoffee); // Find the selected coffee's index value to transfer into updated list.
+    let newMainCoffeeList = this.state.mainCoffeeList.toSpliced(selectedCoffeeIndex, 1, coffeeToEdit); // Update the new list freely, it is not current state yet.
+
+    this.setState({
+      mainCoffeeList: newMainCoffeeList,  // Update the state.
+      editing: false,
+      selectedCoffee: null
+    })
+  };
 
 
   render() {
     let visibleComponent = null;
     let buttonText = null;
 
-    if (this.state.selectedCoffee != null) {
-      visibleComponent = <CoffeeDetail coffee={this.state.selectedCoffee} onCoffeeSale={this.handleCoffeeSale} />
+    if (this.state.editing) {
+      visibleComponent = <EditCoffeeForm onEditCoffee={this.handleEditCoffee} coffee={this.state.selectedCoffee} />
+      buttonText = "Return to Ticket List";
+    }
+    else if (this.state.selectedCoffee != null) {
+      visibleComponent = <CoffeeDetail coffee={this.state.selectedCoffee} onCoffeeSale={this.handleCoffeeSale} onClickEdit={this.handleEditClick} />
       buttonText = "Return to Coffee List"
     }
     else if (this.state.isAddFormVisible) {
