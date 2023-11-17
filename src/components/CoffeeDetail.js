bf632c1d98c6371f4a2f9e1d4e7955b378ed7c12
onurkaymak <onurkaymak@outlook.com>
2023-11-17 11:59:00 -0800

    Add click event listener, update PropTypes.

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CoffeeDetail = (props) => {
-  const { coffee } = props;
+  const { coffee, onCoffeeSale } = props;
 
   return (
     <React.Fragment>
@@ -13,13 +13,14 @@ const CoffeeDetail = (props) => {
       <div>Price: ${coffee.price}</div>
       <div>Quantity: {coffee.quantity} lb</div>
       <hr />
-      <button>Sell Coffee (1lb)</button>
+      <button onClick={() => onCoffeeSale(coffee.id)}>Sell Coffee (1lb)</button>
     </React.Fragment>
   );
 }
 
 CoffeeDetail.propTypes = {
   coffee: PropTypes.object,
+  onCoffeeSale: PropTypes.func
 };
 
 
