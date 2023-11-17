2ee7a799eb17756283867d0af5d0d781e28d7e54
onurkaymak <onurkaymak@outlook.com>
2023-11-17 08:57:28 -0800

    Create Header component, add Header in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>It works!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <h1>Inventory Tracker</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
