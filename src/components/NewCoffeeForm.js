57f173435b9e3cdf56518618f4d147c5b19c1057
onurkaymak <onurkaymak@outlook.com>
2023-11-17 09:25:26 -0800

    Create NewCoffeeForm component.

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from "../utils/ReusableForm";
+
+const NewCoffeeForm = (props) => {
+
+  const handleNewCoffeeFormSubmission = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewCoffeeFormSubmission}
+        buttonText="Add New Coffee" />
+    </React.Fragment>
+  );
+}
+
+NewCoffeeForm.propTypes = {
+
+};
+
+export default NewCoffeeForm;
