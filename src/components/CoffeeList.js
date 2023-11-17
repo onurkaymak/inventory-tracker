fbb4866d4addd179a147072ebf34758eec0e6999
onurkaymak <onurkaymak@outlook.com>
2023-11-17 10:38:05 -0800

    Map coffeeList array that coming from CoffeeControl by props.

--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -1,18 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Coffee from "./Coffee";
 
 const CoffeeList = (props) => {
 
 
   return (
     <React.Fragment>
-
+      <hr />
+      {props.coffeeList.map((coffee) =>
+        <Coffee
+          id={coffee.id}
+          key={coffee.id}
+          name={coffee.name}
+          origin={coffee.origin}
+          price={coffee.price}
+          roast={coffee.roast}
+        />)}
     </React.Fragment>
   );
 }
 
 CoffeeList.propTypes = {
-
+  coffeeList: PropTypes.array
 };
 
 export default CoffeeList;
