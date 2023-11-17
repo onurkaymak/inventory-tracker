34962a32c42f806b2d9d9ab8470c97a14cc6650f
onurkaymak <onurkaymak@outlook.com>
2023-11-17 13:48:58 -0800

    Add conditional check for Further Exploration(Out of Stock).

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -46,14 +46,21 @@ class CoffeeControl extends React.Component {
     // When we use the method shown in Help Queue project, selected item changes its position in the list after edit its state, I think that's bad UI experience. 
     //I implemented a way to keep its position at the list after any changes in the state.
 
-    let selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0]; // Find the selected coffee by id.
-    const selectedCoffeeIndex = this.state.mainCoffeeList.indexOf(selectedCoffee); // Find the selected coffee's index value to transfer into updated list.
-    let newMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== id); // Create an new list without selected coffee.
-    selectedCoffee.quantity--; // Let's sell some coffee!
-    newMainCoffeeList.splice(selectedCoffeeIndex, 0, selectedCoffee); // Add selectedCoffee with updated state into new list.
+    let selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0]; // 1 - Find the selected coffee by id.
+    const selectedCoffeeIndex = this.state.mainCoffeeList.indexOf(selectedCoffee); // 2 - Find the selected coffee's index value to transfer into updated list.
+    let newMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== id); // 3 - Create an new list without selected coffee.
+
+    if (selectedCoffee.quantity === 1) {  // 4 - Let's sell some coffee.
+      selectedCoffee.quantity--;
+      selectedCoffee.isOutOfStock = true; // 5 - Looks like we sold out.
+    } else {
+      selectedCoffee.quantity--;
+    }
+
+    newMainCoffeeList.splice(selectedCoffeeIndex, 0, selectedCoffee); // 6 - Add selectedCoffee with updated state into new list.
 
     this.setState({
-      mainCoffeeList: newMainCoffeeList // Update the state without break any state rules.
+      mainCoffeeList: newMainCoffeeList // 7 - Update the state without break any state rules.
     })
 
   }
